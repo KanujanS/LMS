d1c1cecdd406defeea25bccba5a52e088ce989fc
KanujanS <kanujansk@gmail.com>
2025-03-28 09:26:48 +0530

    Educator dashboard completed

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import { assets } from "../../assets/assets";
 
 const Footer = () => {
-  return (
-    <div>
-      <Footer classname='flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t'>
+  return (    
+      <footer className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t">
         <div className="flex items-center gap-4">
-          <img src={assets.logo} alt="logo" className="hidden md:block w-20"/>
+          <img className="hidden md:block w-20" src={assets.logo} alt="logo" />
           <div className="hidden md:block h-7 w-px bg-gray-500/60"></div>
           <p className="py-4 text-center text-xs md:text-sm text-gray-500">Copyright 2025 © SoftTech.All Rights Reserved</p>
         </div>
@@ -21,8 +20,7 @@ const Footer = () => {
             <img src={assets.instagram_icon} alt="Instagram icon" />
           </a>
         </div>
-      </Footer>
-    </div>
+      </footer>
   );
 };
 
