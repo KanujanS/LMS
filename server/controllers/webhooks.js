16ab4fe14c632efc0130469085638acb4a6cf6d1
KanujanS <kanujansk@gmail.com>
2025-04-06 14:24:32 +0530

    backend completed

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -93,6 +93,7 @@ export const stripeWebhooks = async(request, response)=>{
         await purchaseData.save()
 
         break;}
+        
       case 'payment_intent.payment_failed':{
         const paymentIntent = event.data.object;
         const paymentIntentId = paymentIntent.id;
