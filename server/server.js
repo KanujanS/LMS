42b6eb3a6c09f08c04aab8d1b4f2d47064cc2d1a
KanujanS <kanujansk@gmail.com>
2025-04-14 14:16:45 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -4,10 +4,12 @@ import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
 import { clerkWebhooks, stripeWebhooks } from "./controllers/webhooks.js";
 import educatorRouter from "./routes/educatorRoutes.js";
-import { clerkMiddleware } from "@clerk/express";
 import connectCloudinary from "./configs/cloudinary.js";
 import courseRouter from "./routes/courseRoute.js";
 import userRouter from "./routes/userRoutes.js";
+
+// Import Clerk using require for better Vercel compatibility
+const { clerkMiddleware } = await import('@clerk/express');
 
 // Initialize Express
 const app = express();
